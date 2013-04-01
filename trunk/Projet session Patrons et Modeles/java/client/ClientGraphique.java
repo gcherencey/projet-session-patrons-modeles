e308f3bb01e915ba1c87904d85fe4830a265f879
v.bremond.10@groupe-esigelec.fr <v.bremond.10@groupe-esigelec.fr@be707804-692f-693c-af26-cb444c8e8099>
2013-04-01 22:04:13 +0000

    Client terminé (à priori).

--- trunk/Projet session Patrons et Modeles/java/client/ClientGraphique.java
+++ trunk/Projet session Patrons et Modeles/java/client/ClientGraphique.java
@@ -1,12 +1,18 @@
 package client;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 
 
@@ -19,10 +25,15 @@ import javax.swing.JPanel;
  * @version 1.0
  *
  */
-public class ClientGraphique extends JFrame implements ActionListener
+public class ClientGraphique extends JFrame implements ActionListener,Observer
 {
+	private static final long serialVersionUID = 1L;
+	
+	private Client client;
+	
 	private JButton boutonFermer;
-	private JPanel panelPrincipal;
+	private JPanel panelPrincipal, panelScrollPane;
+	private JScrollPane scrollPane;
 	
 	
 	
@@ -31,19 +42,38 @@ public class ClientGraphique extends JFrame implements ActionListener
 	
 	
 	/**
-	 * Constructeur par défaut.
+	 * Constructeur par valeurs.
+	 * 
+	 * @param client Le Client lié à cette instance.
 	 */
-	public ClientGraphique ()
+	public ClientGraphique (Client client)
 	{
-		super ("Informations");
+		// On demande à être informé à chaque nouveau message reçu
+		this.client = client;
+		client.addObserver (this);
+		
+		// On construit la fenêtre
+		this.setTitle ("Informations");
 		
 		panelPrincipal = new JPanel ();
 		
 		this.add (panelPrincipal);
+		this.setContentPane (panelPrincipal);
+		panelPrincipal.setLayout (new BorderLayout ());
+		
+		panelScrollPane = new JPanel ();
+		panelScrollPane.setLayout (new BoxLayout (panelScrollPane, BoxLayout.PAGE_AXIS));
+		
+		scrollPane = new JScrollPane (panelScrollPane);
+		scrollPane.setPreferredSize (new Dimension (400, 420));
+		scrollPane.setHorizontalScrollBarPolicy (JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy (JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		panelPrincipal.add (scrollPane, BorderLayout.NORTH);
 		
 		boutonFermer = new JButton ("Fermer");
 		boutonFermer.addActionListener (this);
-		panelPrincipal.add (boutonFermer);
+		panelPrincipal.add (boutonFermer, BorderLayout.CENTER);
 		
 		this.setSize (500, 500);
 		this.setLocationRelativeTo (null);
@@ -64,7 +94,11 @@ public class ClientGraphique extends JFrame implements ActionListener
 		
 		if (evenement == boutonFermer)
 		{
+			// Si l'utilisateur veut fermer la fenêtre, on enlève l'observateur et on ferme tout
+			client.deleteObserver (this);
+			
 			this.dispose ();
+			
 			System.exit (0);
 		}
 		
@@ -74,6 +108,16 @@ public class ClientGraphique extends JFrame implements ActionListener
 	
 	public static void main (String[] args)
 	{
-		new ClientGraphique ();
+		// TODO: remove
+		//new ClientGraphique (new Client ());
+	}
+
+
+
+	@Override
+	public void update (Observable arg0, Object arg1)
+	{
+		// Dès que le client reçoit une nouvelle information, on l'affiche
+		panelScrollPane.add (new JLabel ((String) arg1));
 	}
 }
