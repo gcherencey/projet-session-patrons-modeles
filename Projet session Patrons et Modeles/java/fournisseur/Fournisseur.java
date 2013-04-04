2a21468a24a85de1dfbf134fb3e8475200495339
v.bremond.10@groupe-esigelec.fr <v.bremond.10@groupe-esigelec.fr@be707804-692f-693c-af26-cb444c8e8099>
2013-04-04 23:57:51 +0000

    Code propre.

--- Projet session Patrons et Modeles/java/fournisseur/Fournisseur.java
+++ Projet session Patrons et Modeles/java/fournisseur/Fournisseur.java
@@ -20,43 +20,46 @@ import javax.xml.ws.WebServiceException;
  * @version 1.0
  *
  */
-
 public class Fournisseur
 {
-
 	/**
 	 * Permet de demarrer le fournisseur
 	 * @param args
 	 */
 	public static void main (String[] args) throws MalformedURLException, InterruptedException
 	{
-				
-		try {
-				// On cree l'URL
-				URL url = new URL("http://localhost:9998/broker?wsdl");
-				// On cree le qname
-			    QName qname = new QName("http://broker/", "BrokerImplementationService");
-				// On cree le service
-			    Service service = Service.create(url, qname);
-			    //On cree une instance de l'interface broker
-			    BrokerInterface broker = service.getPort (BrokerInterface.class); 
-			    
-			    while(true)
-			    {
-		    	
-			    	//Toutes les secondes on envoie la nouvelle information au broker
-			    	for(int i=0; i<10; i++){
-			    		broker.envoyerInformation("Information : " + i );
-			    		Thread.sleep(1000);
-			    	}
-			    }
-			    
-			} catch (MalformedURLException e) {
-				System.err.println("URL invalide");
-			}catch (WebServiceException e) {
-				System.err.println("Plus de broker connecte");
-			}
-
+		try
+		{
+			// On cree l'URL
+			URL url = new URL("http://localhost:9998/broker?wsdl");
+			
+			// On cree le qname
+		    QName qname = new QName("http://broker/", "BrokerImplementationService");
+			
+		    // On cree le service
+		    Service service = Service.create(url, qname);
+		    
+		    //On cree une instance de l'interface broker
+		    BrokerInterface broker = service.getPort (BrokerInterface.class); 
+		    
+		    while(true)
+		    {
+	    	
+		    	//Toutes les secondes on envoie la nouvelle information au broker
+		    	for(int i=0; i<10; i++)
+		    	{
+		    		broker.envoyerInformation ("Information : " + i );
+		    		Thread.sleep(1000);
+		    	}
+		    }
+		}
+		catch (MalformedURLException e)
+		{
+			System.err.println ("URL invalide");
+		}
+		catch (WebServiceException e)
+		{
+			System.err.println ("Plus de broker connecte");
+		}
 	}
-
 }
